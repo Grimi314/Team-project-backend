52d869e14349dccc37ede6097de8c7c3209c4588
Maryan Serhii <sergijmara210@gmail.com>
2026-07-03 18:47:26 +0300

    initioal commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import pino from "pino-http";
+import "dotenv/config";
+
+import { connectMongoDB } from "./db/connectMongoDB.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(pino());
+
+const PORT = process.env.PORT || 3000;
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
