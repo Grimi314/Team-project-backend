0977a1281edb01a2c3095d5f1dc612a3e2005558
mykhaito <163899158+mykhaito@users.noreply.github.com>
2026-07-05 23:57:31 +0200

    BE-10 req done

--- src/models/story.js
+++ src/models/story.js
@@ -1,1 +1,52 @@
+import mongoose from "mongoose";
 
+const { Schema, model, models } = mongoose;
+
+const storySchema = new Schema(
+  {
+    img: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 40,
+    },
+    article: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 12,
+      maxlength: 3000,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      index: true,
+    },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    rate: {
+      type: Number,
+      default: 0,
+      min: 0,
+      index: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+export const Story = models.Story || model("Story", storySchema, "articles");
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import pino from 'pino-http';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
+import recommendedStoriesRouter from './routes/recommendedStoriesRoutes.js';
 import authRouter from './routes/authRoutes.js';
 
 const app = express();
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(pino());
 
 app.use('/api/auth', authRouter);
+app.use('/api/stories', recommendedStoriesRouter);
 
 const PORT = process.env.PORT || 3000;
 
