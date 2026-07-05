4090ed56ecd305cc9253a177f30f0599829668f0
Oleh <vdovenko.oleg.1983@gmail.com>
2026-07-05 15:18:50 +0300

    add Story model

--- src/models/story.js
+++ src/models/story.js
@@ -3,12 +3,38 @@ import { model } from "mongoose";
 
 const storySchema = new Schema(
   {
-    img: { type: String },
-    ownerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    img: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    article: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    rate: {
+      type: Number,
+      default: 0,
+    },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
   },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
+  { timestamps: true },
 );
 export const Story = model("Story", storySchema, "articles");
